977c81a6133de5bef62e983e8447bd2bb2c76f81
benziony <benziony@perfectomobile.com>
2018-01-25 17:31:45 +0200

    home page btns

--- src/main/java/com/quantum/pages/HomePage.java
+++ src/main/java/com/quantum/pages/HomePage.java
@@ -1,9 +1,26 @@
 package com.quantum.pages;
 
+import com.qmetry.qaf.automation.ui.annotations.FindBy;
 import com.qmetry.qaf.automation.ui.api.PageLocator;
+import com.qmetry.qaf.automation.ui.webdriver.QAFWebElement;
 import com.quantum.utils.DeviceUtils;
 
 public class HomePage extends AbstractBasePage {
+
+    @FindBy(locator = "home.apps/leases.button")
+    QAFWebElement appLeaseBtn;
+
+    @FindBy(locator = "home.apps/tenants.button")
+    QAFWebElement appTenantsBtn;
+
+    @FindBy(locator = "home.properties.button")
+    QAFWebElement propertiesBtn;
+
+    @FindBy(locator = "home.units.button")
+    QAFWebElement unitsBtn;
+
+    @FindBy(locator = "home.rentalOwners.button")
+    QAFWebElement rentalOwnersBtn;
 
     public HomePage(){
         super();
@@ -19,5 +36,19 @@ public class HomePage extends AbstractBasePage {
 
     }
     
-
+    public void navApplicationsLeases(){
+        appLeaseBtn.click();
+    }
+    public void navapplicationsTenants(){
+        appTenantsBtn.click();
+    }
+    public void navProperties(){
+        propertiesBtn.click();
+    }
+    public void navUnits(){
+        unitsBtn.click();
+    }
+    public void navRentalOwners(){
+        rentalOwnersBtn.click();
+    }
 }
